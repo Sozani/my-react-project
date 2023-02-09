4b9713f7ff66fa2f188ff2041bd6bf4f5bd079f1
sozani <sys612603@gmail.com>
2023-02-09 20:16:19 +0200

    trainning

--- src/store/index.js
+++ src/store/index.js
@@ -9,14 +9,10 @@ const counterReducer = (state = { counter: 0 }, action) => {
   }
   return state;
 };
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return { counter: state.counter + 1 };
-    if (action.type === "decrement") {
-      return { counter: state.counter - 1 };
-    }
-    return state;
-  }
-};
+
+
+const store = createStore(counterReducer);
+
+export default store;
 const store = createStore(counterReducer);
 export default store;
