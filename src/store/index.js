5421be105d6e70599d1e97c102ed386c984b426f
sozani <sys612603@gmail.com>
2023-02-09 02:11:43 +0200

    add counterReducer to create store

--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,6 @@ const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "decrement") {
     return { counter: state.counter - 1 };
   }
+  return state;
 };
-const store = createStore();
+const store = createStore(counterReducer);
