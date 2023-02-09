4accb8f2abce25a0def435322bc3820b9df5e072
sozani <sys612603@gmail.com>
2023-02-09 02:03:09 +0200

    add if statment to counterReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,10 @@
 import { createStore } from "redux";
-const counterReducer = (state = { counter: 0 }, action) => {};
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "decrement") {
+    return { counter: state.counter - 1 };
+  }
+};
 const store = createStore();
