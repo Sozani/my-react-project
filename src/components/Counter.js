2d8cf100d50a9edb7bcc6f5c774a5d5410065ff0
sozani <sys612603@gmail.com>
2023-02-09 20:53:07 +0200

    add data to counter component

---
+++ src/components/Counter.js
@@ -0,0 +1,9 @@
+import classes from "./Counter.module.css";
+import React from "react";
+
+const Counter = () => {
+  const toggleCounterHandle = () => {};
+  return <div></div>;
+};
+
+export default Counter;
