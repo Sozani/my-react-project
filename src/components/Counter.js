2e661e53725536fdaf475bc26a8dd245b6e997ad
sozani <sys612603@gmail.com>
2023-02-09 22:19:05 +0200

    utilize useSlector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,14 @@
 import classes from "./Counter.module.css";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
   const toggleCounterHandle = () => {};
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>--Counter Value--</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandle}>Toggle Counter</button>
     </main>
   );
