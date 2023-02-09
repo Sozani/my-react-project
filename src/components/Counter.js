5aba0523193ad963c98030275ae15b30ca55c10a
sozani <sys612603@gmail.com>
2023-02-10 01:06:08 +0200

    dispatch type toggle

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,9 @@ import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
