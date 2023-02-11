b1d9827af1f7e952f5be0839f824b8bf9c572a38
sozani <sys612603@gmail.com>
2023-02-11 23:06:35 +0200

    clear import data

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,6 @@
 import classes from "./Counter.module.css";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { initializeUseSelector } from "react-redux/es/hooks/useSelector";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
