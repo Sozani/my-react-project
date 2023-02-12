6c3a40ff0cf0504c2373c91c5a9753a7afd8d0c0
sozani <sys612603@gmail.com>
2023-02-12 02:36:48 +0200

    Export authActions

--- src/store/index.js
+++ src/store/index.js
@@ -38,5 +38,5 @@ const store = configureStore({
   reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 export const counterActions = counterSlice.actions;
-
+export const authActions = authSlice.actions;
 export default store;
